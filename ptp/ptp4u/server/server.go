df57f86f4d3a670f632a9cf325c947f28486a9f4
Oleg Obleukhov <leoleovich@fb.com>
2022-07-25 18:59:13 +0100

    [ptp4u] ugly hack for faster ptp4l restart

--- ptp/ptp4u/server/server.go
+++ ptp/ptp4u/server/server.go
@@ -370,12 +370,22 @@ func (s *Server) handleGeneralMessages(generalConn *net.UDPConn) {
 							continue
 						}
 
+						// Send confirmation grant
+						s.sendGrant(sc, signaling, v.MsgTypeAndReserved, v.LogInterMessagePeriod, v.DurationField, gclisa)
+
 						if !sc.Running() {
 							go sc.Start(s.ctx)
 						}
 
-						// Send confirmation grant
-						s.sendGrant(sc, signaling, v.MsgTypeAndReserved, v.LogInterMessagePeriod, v.DurationField, gclisa)
+						// Send 3 announce messages right away to unblock ptp4l
+						// https://github.com/richardcochran/linuxptp/blob/ef9ba9489c2f664ea34e5e4dbddbb76cddef5254/foreign.h#L28
+						// https://github.com/richardcochran/linuxptp/blob/33ac7d25cd9212e79be6f7023ba18cfa5020e35b/port.c#L2546
+						if signaling.Header.SequenceID == 0 && grantType == ptp.MessageAnnounce {
+							sc.Once()
+							sc.Once()
+							sc.Once()
+						}
+
 					default:
 						log.Errorf("Got unsupported grant type %s", grantType)
 					}
